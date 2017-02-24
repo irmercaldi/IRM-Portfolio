d021950fabe1d9e7e42ef85b4c25a5cf7def2366
Ian <irmercaldi@gmail.com>
2017-02-24 15:33:56 -0700

    Configured custom routes for pages

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -43,11 +43,13 @@ class PortfoliosController < ApplicationController
     # Perform the lookup
     @portfolio_item = Portfolio.find(params[:id])
 
-    # Destroy the record
+    # Destroy/delete the record
     @portfolio_item.destroy
 
     # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+    end
   end
+
 end
