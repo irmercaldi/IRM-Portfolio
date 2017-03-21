871855c80367fd4707648d7df18cd95830bef44f
Ian <irmercaldi@gmail.com>
2017-03-21 09:33:03 -0600

    further attempts to fix bug in blog show and edit page

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,7 +10,7 @@ class BlogsController < ApplicationController
     if logged_in?(:site_admin)
       @blogs = Blog.recent.page(params[:page]).per(5)
     else
-      @blogs = Blog.published.page(params[:page]).per(5)
+      @blogs = Blog.published.recent.page(params[:page]).per(5)
     end
     @page_title = "My Portfolio Blog"
   end
@@ -25,7 +25,7 @@ class BlogsController < ApplicationController
       @page_title = @blog.title
       @seo_keywords = @blog.body
     else
-      redirect_to blogs_path, notice: "You are not authorized to access this page."
+      redirect_to blogs_path, notice: "You are not authorized to access this page"
     end
   end
 
